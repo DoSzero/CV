5b8ec614963252e0013c25b0709eae21ed9f747d
Dima Kopyl <dimakopyl22@gmail.com>
2021-11-23 22:20:44 +0200

    Personal website

---
+++ js/translate.js
@@ -0,0 +1,198 @@
+// UA 
+let ua_name = "Копил Дмитро "
+let ua_short_bio = "20-yo, <b> Junior Android Developer </b> <br>з Києва, Україна"
+let ua_about_h1 = "Про мене";
+let ua_about_edu = 'Навчаюсь на 4-тому курсі факультеті Kомпютерних наук в <a href="https://nubip.edu.ua/en/node/8734" target="_blank"> National University of Life and Environmental Sciences of Ukraine (NULES). <b> До того навчався у (Коледжі від НАУ), а саме <a href="http://ccte.nau.edu.ua/" target="_blank"> Kyiv College of Computer Technology and Economics of the National Aviation University (CCTE NAU) , факультет Компютерної Інженерії закінчив з відзнакою </b> </a>'
+let ua_about_me = 'Останній рік займаюсь розробкою під Android, опановую Kotlin й сучасний підхід до розробки Android Jetpack. До того займався розробкою Java застосунків (Spring/Fx/Games). Приймаю активну участь в цікавих проєктах.'
+//
+let ua_about_read = '<b>Технічна література яка була прочитана:</b> Head First Java , Java эффективное программирование , Introduction to Programming with Greenfoot: Object-Oriented Programming in Java with Games and Simulations 2nd Edition.'
+let ua_about_fun_fact = '<b>Fun Fact:</b> У 15 років, написав першу гру на Java Greenfoot. Створював модифікації під ігри Paradox Interactive 💎'
+let ua_about_my_works = 'Мої роботи і мою активність можна побачити за посиланнями нижче'
+
+let ua_stack_h1 = "Стек"
+let ua_stack_desc = "Технології, які я активно використовую або був досвід роботи з ними :"
+
+let ua_l1 = "Українська - Носій "
+let ua_l2 = "Російська - Рівень Носія "
+let ua_l3 = "Англійська - B2 "
+
+let ua_stack_h2_1 = "Загальне"
+let ua_stack_h2_2 = "Детальніше"
+
+let ua_work_exp = "Досвід роботи"
+let ua_work_desc = "Маю бажання навчатись новому, покращувати знання і працювати в цікавих проектах. <br> <b>🧑🏻‍💻 Відкритий для пропозицій </b> Office Kyiv / Remote Job"
+let ua_pdf = "Завантажити більш детальну PDF-копію"
+
+
+// RU
+let ru_name = "Дмитрий Копыл";
+let ru_short_bio = "20-yo, <b>Junior Android Developer</b><br>из Киева, Украина";
+
+let ru_about_h1 = "Обо мне";
+let ru_about_edu = 'Обучаюсь на 4-м курсе факультета Компьютерных наук в <a href="https://nubip.edu.ua/en/node/8734" target="_blank"> Национом Университе́те Биоресурсов и Природопользования (NULES). <b> До этого учился в (Колледже от НАУ), а именно <a href="http://ccte.nau.edu.ua/" target="_blank"> Kyiv College of Computer Technology and Economics of National Aviation University (CCTE NAU), факультет Компьютерной Инженерии окончил с отличием</b> </a>';
+
+let ru_about_me = 'Последний год занимаюсь разработкой под Android, овладеваю Kotlin и современным подходом к разработке Android Jetpack. До этого занимался разработкой Java приложений (Spring/Fx/Games). Принимаю активное участие в интересных проектах'
+let ru_about_read = 'Техническая литература которая была прочитана Head First Java , Java эффективное программирование , Introduction to Programming with Greenfoot: Object-Oriented Programming in Java with Games and Simulations 2nd Edition.'
+
+let ru_about_fun_fact = '<b>Fun Fact:</b> В 15 лет, написал первую игру на Java Greenfoot. Создавал модификации под игры Paradox Interactive 💎'
+let ru_about_my_works = 'Мои работы и мою активность можно увидеть по ссылкам ниже'
+
+let ru_stack_h1 = "Стек"
+let ru_stack_desc = "Технологии, которые я активно использую или был опыт работы с ними :"
+
+let ru_l1 = "Украинский - Носитель"
+let ru_l2 = "Русский - Уровень Носителя"
+let ru_l3 = "Английский - B2 "
+
+let ru_stack_h2_1 = "Общее"
+let ru_stack_h2_2 = "Подробнее"
+
+let ru_work_exp = "Опыт работы"
+let ru_work_desc = "Имею огромное желание учиться новому, улучшать знания и работать в интересных проектах.<br><b>🧑🏻‍💻 Открыт для предложений</b> Office Kyiv / Remote Job "
+let ru_pdf = "Скачать более подробную PDF-копию"
+
+
+// ENG 
+let en_name = "Dmytro Kopyl ";
+let en_short_bio = "20-yo, <b>Junior Android Developer</b><br> from Kyiv, Ukraine";
+
+let en_about_h1 = "About me";
+let en_about_edu = 'I am studying in the 4th year of the Faculty of Computer Science at <a href="https://nubip.edu.ua/en/node/8734" target="_blank">The National University of Life and Environmental Sciences of Ukraine (NULES) . <b> Before that he studied at (College of NAU), namely <a href = "http://ccte.nau.edu.ua/ "target =" _ blank "> Kyiv College of Computer Technology and Economics of the National Aviation University (CCTE NAU), Faculty of Computer Engineering graduated with honors </b> </a> ';
+
+let en_about_me = 'For the last year, I have been doing Android development, mastering Kotlin and the modern approach to Android Jetpack development. Before that, he was developing Java applications (Spring / Fx / Games). I take an active part in interesting projects'
+let en_about_read = 'The technical literature was read Head First Java, Java Programming Effectively, Introduction to Programming with Greenfoot: Object-Oriented Programming in Java with Games and Simulations 2nd Edition.'
+
+let en_about_fun_fact = '<b> Fun Fact: </b> At age 15, I wrote my first game on Java Greenfoot. Created modifications for Paradox Interactive games 💎 '
+let en_about_my_works = 'You can see my work and my activity at the links below'
+
+let en_stack_h1 = "Stack"
+let en_stack_desc = "Technologies that I actively use or have had experience with :"
+
+let en_l1 = "Ukrainian - Native "
+let en_l2 = "Russian - Native Level"
+let en_l3 = "English - B2 "
+
+let en_stack_h2_1 = "General"
+let en_stack_h2_2 = "More"
+
+let en_work_exp = "Work experience"
+let en_work_desc = "I have a huge interest to learn new things, improve my knowledge and work in interesting projects.<br><b>🧑🏻‍💻 Open to suggestions</b> Office Kyiv / Remote Job "
+let en_pdf = "Download a more detailed PDF copy"
+
+$(function() {
+    updateText()
+
+    $('#ru').click(function() {
+        window.location.hash = "ru"
+        updateText()
+    });
+    $('#en').click(function() {
+        window.location.hash = "en"
+        updateText()
+    });
+    $('#ua').click(function() {
+        window.location.hash = "ua"
+        updateText()
+    });
+
+});
+
+function updateText() {
+    $(window).scrollTop(0);
+    let lang = window.location.hash.split('#')[1]
+
+    if(lang === "ua"){
+
+        $("#ru").html("RU")
+        $("#ua").html("<b>UA</b>")
+        $("#en").html("EN")
+
+        $("#name").html(ua_name)
+        $("#short_bio").html(ua_short_bio)
+
+        $("#about_h1").html(ua_about_h1)
+        $("#about_edu").html(ua_about_edu)
+        $('#about_me').html(ua_about_me)
+        $("#about_read").html(ua_about_read)
+        $("#about_fun_fact").html(ua_about_fun_fact)
+        $('#about_my_works').html(ua_about_my_works)
+
+        $("#stack_h1").html(ua_stack_h1)
+        $("#stack_description").html(ua_stack_desc)
+        $("#l1").html(ua_l1)
+        $("#l2").html(ua_l2)
+        $("#l3").html(ua_l3)
+
+        $("#stack_h2_1").html(ua_stack_h2_1)
+        $("#stack_h2_2").html(ua_stack_h2_2)
+
+        $("#work_exp").html(ua_work_exp)
+        $("#work_desc").html(ua_work_desc)
+
+        $("#pdf").html(ua_pdf)
+
+    }else if(lang === "en"){
+
+        $("#ru").html("RU")
+        $("#ua").html("UA")
+        $("#en").html("<b>EN</b>")
+
+
+        $("#name").html(en_name)
+        $("#short_bio").html(en_short_bio)
+
+        $("#about_h1").html(en_about_h1)
+        $("#about_edu").html(en_about_edu)
+        $('#about_me').html(en_about_me)
+        $("#about_read").html(en_about_read)
+        $("#about_fun_fact").html(en_about_fun_fact)
+        $('#about_my_works').html(en_about_my_works)
+
+
+        $("#stack_h1").html(en_stack_h1)
+        $("#stack_description").html(en_stack_desc)
+        $("#l1").html(en_l1)
+        $("#l2").html(en_l2)
+        $("#l3").html(en_l3)
+
+        $("#stack_h2_1").html(en_stack_h2_1)
+        $("#stack_h2_2").html(en_stack_h2_2)
+
+        $("#work_exp").html(en_work_exp)
+        $("#work_desc").html(en_work_desc)
+
+        $("#pdf").html(en_pdf)
+
+    }else{
+        window.location.hash = "ru"
+        $("#ru").html("<b>RU</b>")
+        $("#ua").html("UA")
+        $("#en").html("EN")
+
+        $("#name").html(ru_name)
+        $("#short_bio").html(ru_short_bio)
+
+        $("#about_h1").html(ru_about_h1)
+        $("#about_edu").html(ru_about_edu)
+        $('#about_me').html(ru_about_me)
+        $("#about_read").html(ru_about_read)
+        $("#about_fun_fact").html(ru_about_fun_fact)
+        $('#about_my_works').html(ru_about_my_works)
+
+
+        $("#stack_h1").html(ru_stack_h1)
+        $("#stack_description").html(ru_stack_desc)
+        $("#l1").html(ru_l1)
+        $("#l2").html(ru_l2)
+        $("#l3").html(ru_l3)
+
+        $("#stack_h2_1").html(ru_stack_h2_1)
+        $("#stack_h2_2").html(ru_stack_h2_2)
+
+        $("#work_exp").html(ru_work_exp)
+        $("#work_desc").html(ru_work_desc)
+
+        $("#pdf").html(ru_pdf)
+
+    }
+}
